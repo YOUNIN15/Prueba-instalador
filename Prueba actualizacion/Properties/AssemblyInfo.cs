49dbc7d1b584b3163bf93b88854afb7b2a88607a
YOUNIN15 <youninyounin8@gmail.com>
2023-04-28 18:09:36 -0300

    Iniciar publicacion

--- Prueba actualizacion/Properties/AssemblyInfo.cs
+++ Prueba actualizacion/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Prueba actualizacion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("YOUNINDEV")]
 [assembly: AssemblyProduct("Prueba actualizacion")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
