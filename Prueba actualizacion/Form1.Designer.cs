e8756ecd7c3abb9221cc61e3146ef39032a5ec40
YOUNIN15 <youninyounin8@gmail.com>
2023-04-28 22:37:14 -0300

    Yeahss

--- Prueba actualizacion/Form1.Designer.cs
+++ Prueba actualizacion/Form1.Designer.cs
@@ -28,57 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.gunaVScrollBar1 = new Guna.UI.WinForms.GunaVScrollBar();
-            this.gunaRadioButton1 = new Guna.UI.WinForms.GunaRadioButton();
-            this.gunaMetroTrackBar1 = new Guna.UI.WinForms.GunaMetroTrackBar();
             this.label1 = new System.Windows.Forms.Label();
-            this.gunaMediumCheckBox1 = new Guna.UI.WinForms.GunaMediumCheckBox();
-            this.gunaMediumRadioButton1 = new Guna.UI.WinForms.GunaMediumRadioButton();
-            this.gunaMediumCheckBox2 = new Guna.UI.WinForms.GunaMediumCheckBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.gunaTransfarantPictureBox1 = new Guna.UI.WinForms.GunaTransfarantPictureBox();
-            this.gunaWinSwitch1 = new Guna.UI.WinForms.GunaWinSwitch();
-            this.gunaSeparator1 = new Guna.UI.WinForms.GunaSeparator();
-            this.gunaTileButton1 = new Guna.UI.WinForms.GunaTileButton();
-            ((System.ComponentModel.ISupportInitialize)(this.gunaTransfarantPictureBox1)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // gunaVScrollBar1
-            // 
-            this.gunaVScrollBar1.LargeChange = 10;
-            this.gunaVScrollBar1.Location = new System.Drawing.Point(173, 93);
-            this.gunaVScrollBar1.Maximum = 100;
-            this.gunaVScrollBar1.Name = "gunaVScrollBar1";
-            this.gunaVScrollBar1.ScrollIdleColor = System.Drawing.Color.Silver;
-            this.gunaVScrollBar1.Size = new System.Drawing.Size(10, 200);
-            this.gunaVScrollBar1.TabIndex = 0;
-            this.gunaVScrollBar1.ThumbColor = System.Drawing.Color.DimGray;
-            this.gunaVScrollBar1.ThumbHoverColor = System.Drawing.Color.Gray;
-            this.gunaVScrollBar1.ThumbPressedColor = System.Drawing.Color.DarkGray;
-            // 
-            // gunaRadioButton1
-            // 
-            this.gunaRadioButton1.BaseColor = System.Drawing.SystemColors.Control;
-            this.gunaRadioButton1.CheckedOffColor = System.Drawing.Color.Gray;
-            this.gunaRadioButton1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaRadioButton1.FillColor = System.Drawing.Color.White;
-            this.gunaRadioButton1.Location = new System.Drawing.Point(242, 198);
-            this.gunaRadioButton1.Name = "gunaRadioButton1";
-            this.gunaRadioButton1.Size = new System.Drawing.Size(124, 20);
-            this.gunaRadioButton1.TabIndex = 2;
-            this.gunaRadioButton1.Text = "gunaRadioButton1";
-            // 
-            // gunaMetroTrackBar1
-            // 
-            this.gunaMetroTrackBar1.Location = new System.Drawing.Point(242, 331);
-            this.gunaMetroTrackBar1.Name = "gunaMetroTrackBar1";
-            this.gunaMetroTrackBar1.Size = new System.Drawing.Size(100, 23);
-            this.gunaMetroTrackBar1.TabIndex = 4;
-            this.gunaMetroTrackBar1.TrackColor = System.Drawing.Color.DimGray;
-            this.gunaMetroTrackBar1.TrackHoverColor = System.Drawing.Color.Gray;
-            this.gunaMetroTrackBar1.TrackIdleColor = System.Drawing.Color.Silver;
-            this.gunaMetroTrackBar1.TrackPressedColor = System.Drawing.Color.Black;
             // 
             // label1
             // 
@@ -88,39 +41,6 @@
             this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Alturomelomama15";
-            // 
-            // gunaMediumCheckBox1
-            // 
-            this.gunaMediumCheckBox1.BaseColor = System.Drawing.Color.White;
-            this.gunaMediumCheckBox1.CheckedOffColor = System.Drawing.Color.Gray;
-            this.gunaMediumCheckBox1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaMediumCheckBox1.FillColor = System.Drawing.Color.White;
-            this.gunaMediumCheckBox1.Location = new System.Drawing.Point(83, 292);
-            this.gunaMediumCheckBox1.Name = "gunaMediumCheckBox1";
-            this.gunaMediumCheckBox1.Size = new System.Drawing.Size(20, 20);
-            this.gunaMediumCheckBox1.TabIndex = 6;
-            // 
-            // gunaMediumRadioButton1
-            // 
-            this.gunaMediumRadioButton1.BaseColor = System.Drawing.Color.White;
-            this.gunaMediumRadioButton1.CheckedOffColor = System.Drawing.Color.Gray;
-            this.gunaMediumRadioButton1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaMediumRadioButton1.FillColor = System.Drawing.Color.White;
-            this.gunaMediumRadioButton1.Location = new System.Drawing.Point(83, 198);
-            this.gunaMediumRadioButton1.Name = "gunaMediumRadioButton1";
-            this.gunaMediumRadioButton1.Size = new System.Drawing.Size(20, 20);
-            this.gunaMediumRadioButton1.TabIndex = 7;
-            // 
-            // gunaMediumCheckBox2
-            // 
-            this.gunaMediumCheckBox2.BaseColor = System.Drawing.Color.White;
-            this.gunaMediumCheckBox2.CheckedOffColor = System.Drawing.Color.Gray;
-            this.gunaMediumCheckBox2.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaMediumCheckBox2.FillColor = System.Drawing.Color.White;
-            this.gunaMediumCheckBox2.Location = new System.Drawing.Point(68, 79);
-            this.gunaMediumCheckBox2.Name = "gunaMediumCheckBox2";
-            this.gunaMediumCheckBox2.Size = new System.Drawing.Size(20, 20);
-            this.gunaMediumCheckBox2.TabIndex = 8;
             // 
             // button1
             // 
@@ -132,98 +52,34 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // gunaTransfarantPictureBox1
+            // label2
             // 
-            this.gunaTransfarantPictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.gunaTransfarantPictureBox1.BaseColor = System.Drawing.Color.Black;
-            this.gunaTransfarantPictureBox1.Location = new System.Drawing.Point(606, 198);
-            this.gunaTransfarantPictureBox1.Name = "gunaTransfarantPictureBox1";
-            this.gunaTransfarantPictureBox1.Size = new System.Drawing.Size(120, 120);
-            this.gunaTransfarantPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.gunaTransfarantPictureBox1.TabIndex = 10;
-            this.gunaTransfarantPictureBox1.TabStop = false;
-            // 
-            // gunaWinSwitch1
-            // 
-            this.gunaWinSwitch1.BaseColor = System.Drawing.SystemColors.Control;
-            this.gunaWinSwitch1.CheckedOffColor = System.Drawing.Color.DarkGray;
-            this.gunaWinSwitch1.CheckedOnColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaWinSwitch1.FillColor = System.Drawing.Color.White;
-            this.gunaWinSwitch1.Location = new System.Drawing.Point(143, 363);
-            this.gunaWinSwitch1.Name = "gunaWinSwitch1";
-            this.gunaWinSwitch1.Size = new System.Drawing.Size(40, 22);
-            this.gunaWinSwitch1.TabIndex = 11;
-            // 
-            // gunaSeparator1
-            // 
-            this.gunaSeparator1.LineColor = System.Drawing.Color.Silver;
-            this.gunaSeparator1.Location = new System.Drawing.Point(439, 128);
-            this.gunaSeparator1.Name = "gunaSeparator1";
-            this.gunaSeparator1.Size = new System.Drawing.Size(200, 10);
-            this.gunaSeparator1.TabIndex = 12;
-            // 
-            // gunaTileButton1
-            // 
-            this.gunaTileButton1.AnimationHoverSpeed = 0.07F;
-            this.gunaTileButton1.AnimationSpeed = 0.03F;
-            this.gunaTileButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaTileButton1.BorderColor = System.Drawing.Color.Black;
-            this.gunaTileButton1.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaTileButton1.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaTileButton1.Font = new System.Drawing.Font("Segoe UI Light", 15.75F);
-            this.gunaTileButton1.ForeColor = System.Drawing.Color.White;
-            this.gunaTileButton1.Image = ((System.Drawing.Image)(resources.GetObject("gunaTileButton1.Image")));
-            this.gunaTileButton1.ImageSize = new System.Drawing.Size(52, 52);
-            this.gunaTileButton1.Location = new System.Drawing.Point(408, 184);
-            this.gunaTileButton1.Name = "gunaTileButton1";
-            this.gunaTileButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.gunaTileButton1.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaTileButton1.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaTileButton1.OnHoverImage = null;
-            this.gunaTileButton1.OnPressedColor = System.Drawing.Color.Black;
-            this.gunaTileButton1.Size = new System.Drawing.Size(160, 160);
-            this.gunaTileButton1.TabIndex = 13;
-            this.gunaTileButton1.Text = "gunaTileButton1";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "This is ROCKET LEAGUE";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.gunaTileButton1);
-            this.Controls.Add(this.gunaSeparator1);
-            this.Controls.Add(this.gunaWinSwitch1);
-            this.Controls.Add(this.gunaTransfarantPictureBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.gunaMediumCheckBox2);
-            this.Controls.Add(this.gunaMediumRadioButton1);
-            this.Controls.Add(this.gunaMediumCheckBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.gunaMetroTrackBar1);
-            this.Controls.Add(this.gunaRadioButton1);
-            this.Controls.Add(this.gunaVScrollBar1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.gunaTransfarantPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Guna.UI.WinForms.GunaVScrollBar gunaVScrollBar1;
-        private Guna.UI.WinForms.GunaRadioButton gunaRadioButton1;
-        private Guna.UI.WinForms.GunaMetroTrackBar gunaMetroTrackBar1;
         private System.Windows.Forms.Label label1;
-        private Guna.UI.WinForms.GunaMediumCheckBox gunaMediumCheckBox1;
-        private Guna.UI.WinForms.GunaMediumRadioButton gunaMediumRadioButton1;
-        private Guna.UI.WinForms.GunaMediumCheckBox gunaMediumCheckBox2;
         private System.Windows.Forms.Button button1;
-        private Guna.UI.WinForms.GunaTransfarantPictureBox gunaTransfarantPictureBox1;
-        private Guna.UI.WinForms.GunaWinSwitch gunaWinSwitch1;
-        private Guna.UI.WinForms.GunaSeparator gunaSeparator1;
-        private Guna.UI.WinForms.GunaTileButton gunaTileButton1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
